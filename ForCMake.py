405aa78c7430fc87709eaf10b3ca1807a3dbfac5
LeoEVEILLARD <l.cecchelani@mail.com>
2018-03-05 10:28:00 +0100

    now working, but to has to be improved

--- ForCMake.py
+++ ForCMake.py
@@ -11,8 +11,7 @@ def FindCMakeLists(dir):
             if file == "CMakeLists.txt":
                 #print("CMakeLists.txt trouvé :", os.path.join(root,file))
                 ListHeaders.append(os.path.join(root,file))
-                dictExternDependency[file] = [];
-                dictInternDependency[file] = [];
+
 
     return ListHeaders, dictExternDependency , dictInternDependency
 
@@ -30,12 +29,16 @@ def GrepCMakeContent(ListHeaders,  dictExternDependency , dictInternDependency):
             print(line)
             if regEx_FindName in line: #name
                name = line[line.find("(")+1:line.find(")")];
+               dictExternDependency[name] = [];
+               dictInternDependency[name] = [];
                print("nom trouvé : ", name)
             elif regEx_FindDependency in line: #dep
                dep = line[line.find("(")+1:line.find(")")];
                print("dep trouvée : ", dep)
                 
         if name != "pastrouve":
+               dictExternDependency[name] = [];
+               dictInternDependency[name] = [];
                dictInternDependency[name].append(dep);
     #print("dependance interne trouvée : ", name)
     return dictExternDependency , dictInternDependency
